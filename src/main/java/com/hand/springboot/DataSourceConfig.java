edf01a153d409771ae99abae0be03aae295550bc
WangPan <wangpanxcx@163.com>
2018-05-17 19:09:29 +0800

    test

---
+++ src/main/java/com/hand/springboot/DataSourceConfig.java
@@ -0,0 +1,36 @@
+package com.hand.springboot;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * @author WangPan
+ * @title DataSourceConfig
+ * @description 多数据源的配置
+ * @date: 2018/5/17 17:56
+ */
+@Configuration
+public class DataSourceConfig {
+    /**
+     * 主数据源配置为spring.datasource.primary开头的配置，第二数据源配置为spring.datasource.secondary开头的配置。
+     */
+    @Bean(name = "primaryDataSource")
+    @Qualifier("primaryDataSource")
+    @ConfigurationProperties(prefix="spring.datasource.primary")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "secondaryDataSource")
+    @Qualifier("secondaryDataSource")
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource.secondary")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
